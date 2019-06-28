f8a281aef3e6fab903bdbf88a3c9363bd50716d3
Jason Ngo <ngojason9@gmail.com>
2019-06-28 10:32:56 -0600

    Restructure project directory

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,18 +1,34 @@
-from fbs_runtime.application_context.PyQt5 import ApplicationContext
+import sys
+from fbs_runtime.application_context.PyQt5 import ApplicationContext, cached_property
 from PyQt5.QtWidgets import QMainWindow
 
-import sys
+from ui.main_window import Ui_MainWindow
 
 
 class AppContext(ApplicationContext):           # 1. Subclass ApplicationContext
     def run(self):                              # 2. Implement run()
-        window = QMainWindow()
-        version = self.build_settings['version']
-        window.setWindowTitle("HelloWorld v" + version)
-        window.resize(250, 150)
-        window.show()
+        self.main_window.show()
         return self.app.exec_()                 # 3. End run() with this line
 
+    @cached_property
+    def main_window(self):
+        return MainWindow(self)
+
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self, ctx):
+        super(MainWindow, self).__init__()
+        self.setupUi(self)
+        self.ctx = ctx
+        
+        self.test_push_button.clicked.connect(self.button_clicked)
+        version = self.ctx.build_settings['version']
+        self.setWindowTitle("DART Plotting Tool v." + version)
+        self.resize(250, 150)
+    
+    def button_clicked(self):
+        self.test_push_button.setText("Button Clicked")
+        print("Button clicked")
 
 if __name__ == '__main__':
     appctxt = AppContext()                      # 4. Instantiate the subclass
