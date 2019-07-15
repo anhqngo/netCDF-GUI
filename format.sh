e4101f0b338d1e035c84f03310726eaefec760f7
Jason Ngo <ngojason9@gmail.com>
2019-07-15 10:59:01 -0600

    rm unused files

--- format.sh
+++
@@ -1,8 +0,0 @@
-# I have to write this command manually because vscode for some reason does
-# not recognize autopep8 tool
-autopep8 --in-place --aggressive --aggressive src/main/python/main.py
-
-for file in $(find src/main/python/utils -name '*.py'); 
-do 
-autopep8 --in-place --aggressive --aggressive $file 
-done
